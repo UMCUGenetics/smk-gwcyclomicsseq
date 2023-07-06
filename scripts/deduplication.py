3c593e286e0dd2a12747f673ec8b38346dd4bb43
zztin <litingchen16@gmail.com>
2023-07-06 14:15:21 +0200

    Add counter for duplication

--- scripts/deduplication.py
+++ scripts/deduplication.py
@@ -1,4 +1,6 @@
 import pysam
+import pandas as pd
+from collections import Counter
 import singlecellmultiomics
 from singlecellmultiomics.molecule import MoleculeIterator
 from singlecellmultiomics.bamProcessing import sorted_bam_file
@@ -23,7 +25,8 @@ def write_sm_tag_to_bam(input_bam, SM_bam, SM_tag_value):
     return SM_bam
 
 
-def write_deduplicate(input_bam_path, target_bam_path, reference, SM_tag_value):
+def write_deduplicate(input_bam_path, target_bam_path, reference, SM_tag_value, merge_max, rca_tag="YM"):
+    unique_molecules_copies = Counter()
     with pysam.AlignmentFile(input_bam_path) as f:
         with sorted_bam_file(target_bam_path, origin_bam=f, ) as target_bam:
             for i, m in tqdm(enumerate(MoleculeIterator(
@@ -33,7 +36,7 @@ def write_deduplicate(input_bam_path, target_bam_path, reference, SM_tag_value):
                     every_fragment_as_molecule=False,
                     perform_qflag=False,
                     molecule_class_args={"reference": reference, "max_associated_fragments": 100},
-                    fragment_class_args={"assignment_radius": 20, "rca_tag": "YM", "sample": SM_tag_value},
+                    fragment_class_args={"assignment_radius": merge_max, "rca_tag": rca_tag, "sample": SM_tag_value},
                     max_buffer_size=1000000,
                     yield_overflow=False,
             ))):
@@ -43,6 +46,17 @@ def write_deduplicate(input_bam_path, target_bam_path, reference, SM_tag_value):
                               consensus_name=read_name,
                               no_source_reads=True,
                               )
+                unique_molecules_copies[len(m)] += 1
+    print(sorted(unique_molecules_copies.items()))
+    ### Write deduplication counter
+    name = target_bam_path.split("/")[-1].split(".")[0]
+    with open(out_bam+"unique_molecule_count.tsv", "w") as f:
+        f.write( f"rep\t{name}\n" )
+        for i in range(1, 101):
+            appears_n_times = i
+            k_molecules = unique_molecules_copies[i]
+            f.write( f"{appears_n_times}\t{k_molecules}\n" )
+    # pd.to_pickle(unique_molecules_copies, out_bam+".pickle.gz")
 
 
 if __name__=="__main__":
@@ -54,8 +68,9 @@ if __name__=="__main__":
     parser.add_argument('--SM', type=str, default=None, help="value to put in SM tag. Can skip is SM tag existed.")
     parser.add_argument('--ref', type=str, help='reference which bam file is mapped to. (str). Auto-detect is possible.')
     parser.add_argument('--merge', action='store_true', help='merge reads from different nanopore reads but covering the same start,'
-                                                  'end sites within max 2bp range. Direction of the read is set by the first added read.')
-
+                                                  'end sites within max X range. Direction of the read is set by the first added read.')
+    parser.add_argument('--merge_max', type= int, default=5,
+                        help='Merge nanopore reads within max X bp range. Strand of the read is the first added read.')
     args = parser.parse_args()
 
     timeA = time.time()
@@ -88,7 +103,7 @@ if __name__=="__main__":
     else:
         input_bam = args.read_bam
 
-    write_deduplicate(input_bam, t_bam, reference, args.SM)
+    write_deduplicate(input_bam, t_bam, reference, args.SM, args.merge_max, rca_tag="YM")
 
     pysam.index(str(t_bam))
 
